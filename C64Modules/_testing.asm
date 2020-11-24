47ba972330826c0a5803b00c0cdfaac8f65fa1c8
Akira-Hojo <69775188+Akira-Hojo@users.noreply.github.com>
2020-11-23 23:50:08 -0500

    Testing 2

---
+++ C64Modules/_testing.asm
@@ -0,0 +1,87 @@
+incasm "kernal.asm"
+incasm "colors.asm"
+incasm "ascii.asm"
+
+; 10 SYS (49152)
+
+*=$0801
+
+        BYTE    $0E, $08, $0A, $00, $9E, $20, $28,  $34, $39, $31, $35, $32, $29, $00, $00, $00
+
+
+*=$C000
+
+;clearly show your intentions
+
+InitScreen
+        lda black
+        sta $d020 ; border
+        sta $d021 ; bg
+        lda CHR_ClearScreen
+        jsr chrout
+        lda CHR_White
+        jsr chrout
+        lda CHR_SwitchToUpperCase
+        jsr chrout
+        
+        ;loop all 0-f
+        ldy #0
+loop    tya
+        jsr printh
+        lda #32
+        jsr chrout
+        iny
+        bne loop
+
+endless jmp endless
+
+printh
+        pha ;push a on stack
+        lsr a ; shift the high bite down to the low bite
+        lsr a ; just ^
+        lsr a ; keep ^ 
+        lsr a ; shifting ^
+        jsr conv4 ;call hex2ascii
+        jsr chrout
+        pla ;pull a from stack
+        and #$0f ;clear high nibble keep low nibble ;%00001111
+        jsr conv4
+        jsr chrout
+        rts
+
+conv1
+        tax ; transfer a to x
+        lda table,x ;lda with petscii value from below table offset by x
+        rts
+
+table   byte 48,49,50,51,52 ;0-4
+        byte 53,54,55,56,57 ;5-9
+        byte 65,66,67,68,69,70 ;A-F
+        ;22 bytes
+
+
+
+conv2
+        cmp #10 ;compare to 10
+        bcc @ahead ;carry clear less than, carry set greater than
+        adc #6 ;carry set adds one extra, 6+carry=7 skip petscii 58-63
+@ahead  adc #"0" ;petscii 0
+        rts
+        ;9 bytes
+
+conv3
+        sed  ;set decimal mode (addition) 00-99 e.g. h32+h45=h77
+        clc
+        adc #$90
+        adc #$40
+        cld
+        rts
+        ;8 bytes
+
+conv4
+        sed
+        cmp #10
+        adc #48
+        cld
+        rts
+        ;7 bytes
---
+++ C64Modules/colormem.asm
@@ -0,0 +1,31 @@
+;*******************************************************************************
+;Color Memory Map
+;*******************************************************************************
+
+;Column Number Starting Adddress - 
+
+COLORCOL1  = $D800             ;55296 top left
+COLORCOL2  = $D828             ;55336
+COLORCOL3  = $D850             ;55376
+COLORCOL4  = $D878             ;55416
+COLORCOL5  = $D8A0             ;55456
+COLORCOL6  = $D8C8             ;55496
+COLORCOL7  = $D8F0             ;55536
+COLORCOL8  = $D918             ;55576
+COLORCOL9  = $D940             ;55616
+COLORCOL10 = $D968             ;55656
+COLORCOL11 = $D990             ;55696
+COLORCOL12 = $D9B8             ;55736
+COLORCOL13 = $D9E0             ;55776
+COLORCOL14 = $DA08             ;55816
+COLORCOL15 = $DA30             ;55856
+COLORCOL16 = $DA58             ;55896
+COLORCOL17 = $DA80             ;55936
+COLORCOL18 = $DAA8             ;55976
+COLORCOL19 = $DAD0             ;56016
+COLORCOL20 = $DAF8             ;56056
+COLORCOL21 = $DB20             ;56096
+COLORCOL22 = $DB48             ;56136
+COLORCOL23 = $DB70             ;56176
+COLORCOL24 = $DB98             ;56216
+COLORCOL25 = $DBC0             ;56256 bottom left
